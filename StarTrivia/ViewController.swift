cc3359631161dc43c0efe0725807c1d7b56c3f12
Jonathan Go <jogo88@gmail.com>
2018-10-13 16:43:18 +0900

    Initial Commit

---
+++ StarTrivia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarTrivia
+//
+//  Created by Jonathan Go on 2018/10/13.
+//  Copyright © 2018 Appdelight. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
