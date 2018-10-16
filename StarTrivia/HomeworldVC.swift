5a7ff25d2667fa9d20e0c8fcee4542d842d7c3ea
Jonathan Go <jogo88@gmail.com>
2018-10-16 16:45:55 +0900

    created VCs and segues for each button.

---
+++ StarTrivia/HomeworldVC.swift
@@ -0,0 +1,30 @@
+//
+//  HomeworldVC.swift
+//  StarTrivia
+//
+//  Created by Jonathan Go on 2018/10/16.
+//  Copyright © 2018 Appdelight. All rights reserved.
+//
+
+import UIKit
+
+class HomeworldVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
