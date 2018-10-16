953032a6eb978c75d574d4125115ff047e49f5b8
Jonathan Go <jogo88@gmail.com>
2018-10-16 14:51:29 +0900

    added json, person struct, personapi, got random button working...

---
+++ StarTrivia/Model/Person.swift
@@ -0,0 +1,22 @@
+//
+//  Person.swift
+//  StarTrivia
+//
+//  Created by Jonathan Go on 2018/10/15.
+//  Copyright © 2018 Appdelight. All rights reserved.
+//
+
+import Foundation
+
+struct Person {
+  let name: String
+  let height: String
+  let mass: String
+  let hair: String
+  let birthYear: String
+  let gender: String
+  let homeworldUrl: String
+  let filmUrls: [String]
+  let vehicleUrls: [String]
+  let starshipUrls: [String]
+}
--- StarTrivia/SelectPersonVC.swift
+++ StarTrivia/SelectPersonVC.swift
@@ -10,12 +10,37 @@ import UIKit
 
 class SelectPersonVC: UIViewController {
 
+  @IBOutlet var nameLbl: UILabel!
+  @IBOutlet var heightLbl: UILabel!
+  @IBOutlet var massLbl: UILabel!
+  @IBOutlet var hairLbl: UILabel!
+  @IBOutlet var birthYearLbl: UILabel!
+  @IBOutlet var genderLbl: UILabel!
+  
+  
+  var personApi = PersonApi()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    
+   
   }
 
-
+  @IBAction func randomBtnPressed(_ sender: Any) {
+    let random = Int.random(in: 1...87) //there are 87 names in SWAPI
+    //Step 1: make networking request
+    personApi.getRandomPersonUrlSession(id: random) { (person) in
+      if let person = person {
+        self.nameLbl.text = person.name
+        self.heightLbl.text = person.height
+        self.massLbl.text = person.mass
+        self.hairLbl.text = person.hair
+        self.birthYearLbl.text = person.birthYear
+        self.genderLbl.text = person.gender
+      }
+    }
+    //
+  }
+  
 }
 
